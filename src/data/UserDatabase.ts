a77958d198d32297c854b0d818475d73f0710bfd
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-09-22 17:41:51 -0300

    Preparação do ambiente, criação dos enpoint signup e login

---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,38 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+    private static TABLE_USERS: string = "UserLabook";
+
+    public async createUser(
+        id: string,
+        name: string,
+        email: string,
+        password: string
+    ): Promise<void> {
+        try {
+            await this.getConnection()
+                .insert({
+                    id,
+                    name,
+                    email,
+                    password
+                }).into(UserDatabase.TABLE_USERS)
+        } catch (error) {
+            throw new Error(error.sqlMessage || error.message)
+        }
+    }
+
+    public async getUserByEmail(email: string): Promise<any> {
+        try {
+            const result = await this.getConnection()
+                    .select('*')
+                    .from(UserDatabase.TABLE_USERS)
+                    .where({email})
+                return result[0]
+        } catch (error) {
+            throw new Error(error.sqlMessage || error.message)
+        }
+    }
+
+    
+}
